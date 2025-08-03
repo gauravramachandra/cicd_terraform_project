fbb6312254bf63e483f53ebd86206bb33149695f
Gaurav R <gau682@gmail.com>
2025-08-03 18:34:18 +0000

    Edit backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mystatebucketterraformproject"
-    key    = "state"
+    key    = "vpc/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "backend"
     encrypt = true
